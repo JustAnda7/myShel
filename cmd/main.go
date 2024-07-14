8d6c65cfb7a5a3cee82b0479d49e2796489566b4
JustAnda7 <justanda7#gmail.com>
2024-07-14 18:23:59 +0530

    add-history-feature

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"os/signal"
@@ -11,39 +12,41 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
-	"syscall"
 )
 
 type (
 	fn func([]string)
 )
 
+type Hist struct {
+	user_cmd []string
+	cmd_file *os.File
+}
+
 var builtin = make(map[string]fn)
 
 func main() {
 	initcmd()
-	sigchnl := make(chan os.Signal, 1)
-	signal.Notify(sigchnl)
-
+	cmd_hist := newHistory()
+	signal.Ignore(os.Interrupt)
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
-		go func() {
-			for {
-				sig := <-sigchnl
-				handler(sig)
-			}
-		}()
 		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			os.Exit(1)
 		}
 		input = strings.TrimRight(input, "\n")
+		cmd_hist.addToHistory(input)
 		cmds := strings.Split(strings.TrimSpace(input), "|")
 		if len(cmds) != 1 {
 			pipedCommand(input)
 		} else {
 			inputs := strings.Split(strings.TrimSpace(input), " ")
 			args := inputs[1:]
+			if inputs[0] == "history" {
+				history(cmd_hist)
+				continue
+			}
 			cmdfc, avail := builtin[inputs[0]]
 			if avail {
 				cmdfc(args)
@@ -67,12 +70,6 @@ func main() {
 	}
 }
 
-func handler(signal os.Signal) {
-	if signal == syscall.SIGINT {
-		fmt.Println("\nType Exit to quit the shell")
-	}
-}
-
 func initcmd() {
 	builtin["echo"] = echo
 	builtin["exit"] = exit
@@ -81,30 +78,79 @@ func initcmd() {
 	builtin["cd"] = cd
 }
 
+func newHistory() *Hist {
+	path, _ := os.UserHomeDir()
+	var hist_file = filepath.Join(path, ".msh_history")
+	var h Hist
+	h.cmd_file, _ = os.OpenFile(hist_file, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0600)
+	// defer h.cmd_file.Close()
+	scanner := bufio.NewScanner(h.cmd_file)
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		h.user_cmd = append(h.user_cmd, scanner.Text())
+	}
+	return &h
+}
+
+func (h *Hist) addToHistory(command string) {
+	if len(h.user_cmd) == 0 || h.user_cmd[len(h.user_cmd)-1] != command {
+		h.user_cmd = append(h.user_cmd, command)
+		h.cmd_file.WriteString(command + "\n")
+	}
+}
+
 func pipedCommand(input string) {
 	input = strings.TrimSpace(input)
-	program := exec.Command("bash", "-c", input)
-	program.Stdout = os.Stdout
-	program.Stderr = os.Stderr
-	if err := program.Run(); err != nil {
-		fmt.Fprintf(os.Stdout, "%s: command not found\n", input)
+	commands := strings.Split(input, "|")
+	var cmds []*exec.Cmd
+	var output io.ReadCloser
+
+	for _, command := range commands {
+		args := strings.Split(strings.TrimSpace(command), " ")
+		program := exec.Command(args[0], args[1:]...)
+		program.Stderr = os.Stderr
+		cmds = append(cmds, program)
+
+		if output != nil {
+			program.Stdin = output
+		}
+		output, _ = program.StdoutPipe()
+	}
+	if len(cmds) > 0 {
+		cmds[len(cmds)-1].Stdout = os.Stdout
+	}
+	for _, cmd := range cmds {
+		cmd.Start()
+	}
+	for _, cmd := range cmds {
+		err := cmd.Wait()
+		if err != nil {
+			if cmd.ProcessState.ExitCode() == -1 {
+				fmt.Fprintf(os.Stdout, "%s: Command not found", input)
+			}
+		}
 	}
 }
 
 func cd(args []string) {
-	p := path.Clean(args[0])
-	if p == "~" {
-		p = os.Getenv("HOME")
-	}
-	if !path.IsAbs(p) {
-		dir, err := os.Getwd()
-		if err != nil {
-			fmt.Fprintf(os.Stdout, "Error: ") //handle error message
+	if len(args) != 1 {
+		fmt.Fprintf(os.Stdout, "Invalid number of Arguments.\n") // add a help
+		return
+	} else {
+		p := path.Clean(args[0])
+		if p == "~" {
+			p = os.Getenv("HOME")
 		}
-		p = path.Join(dir, p)
-	}
-	if err := os.Chdir(p); err != nil {
-		fmt.Fprintf(os.Stdout, "%s: No such file or directory\n", p)
+		if !path.IsAbs(p) {
+			dir, err := os.Getwd()
+			if err != nil {
+				fmt.Fprintf(os.Stdout, "Error: ") //handle error message
+			}
+			p = path.Join(dir, p)
+		}
+		if err := os.Chdir(p); err != nil {
+			fmt.Fprintf(os.Stdout, "%s: No such file or directory\n", p)
+		}
 	}
 }
 
@@ -126,6 +172,14 @@ func exit(args []string) {
 	}
 	if code, err := strconv.Atoi(args[0]); err == nil {
 		os.Exit(code)
+	}
+}
+
+func history(h *Hist) {
+	for _, hc := range h.user_cmd {
+		if hc != "history" {
+			fmt.Printf("- %s\n", hc)
+		}
 	}
 }
 
